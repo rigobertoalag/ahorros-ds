3b4b7678435eb8029525bf6692433ccd2d178d6e
rigobertoalag <a20150338@utem.edu.mx>
2021-06-01 16:57:00 -0500

    Finish with styles in login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { LockClosedIcon } from '@heroicons/react/solid'
 
@@ -22,7 +23,7 @@ export default function Login() {
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
                                 placeholder="Ingresa tu correo electronico"
                             />
                         </div>
@@ -36,7 +37,7 @@ export default function Login() {
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-lg relative block w-full px-3 py-2 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm my-2"
                                 placeholder="Contraseña"
                             />
                         </div>
@@ -44,16 +45,16 @@ export default function Login() {
 
                     <div className="flex items-center justify-between">
                         <div className="text-sm">
-                            <a href="#" className="font-medium text-pink-600 hover:text-pink-500">
+                            <Link to="/register" className="font-medium text-pink-600 hover:text-pink-500">
                                 ¿No tienes cuenta? - Registrate
-                      </a>
+                            </Link>
                         </div>
                     </div>
 
                     <div>
                         <button
                             type="submit"
-                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 my-2"
                         >
                             <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                                 <LockClosedIcon className="h-5 w-5 text-pink-500 group-hover:text-indigo-400" aria-hidden="true" />
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { LockClosedIcon } from '@heroicons/react/solid'
 
@@ -7,46 +8,71 @@ export default function Register() {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Inicio de sesion</h2>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Registrate</h2>
                 </div>
                 <form className="mt-8 space-y-6" action="#" method="POST">
                     <input type="hidden" name="remember" defaultValue="true" />
                     <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
+                        <div className="">
+                            <label htmlFor="name" className="sr-only">
+                                Nombre
+                            </label>
+                            <input
+                                id="name"
+                                name="name"
+                                type="text"
+                                required
+                                className="appearance-none rounded-md relative block w-full px-3 py-3 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm my-2"
+                                placeholder="Ingresa tu nombre"
+                            />
+                        </div>
+                        <div className="">
                             <label htmlFor="email-address" className="sr-only">
                                 Correo
-                      </label>
+                            </label>
                             <input
                                 id="email-address"
                                 name="email"
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm my-2"
                                 placeholder="Ingresa tu correo electronico"
                             />
                         </div>
-                        <div>
-                            <label htmlFor="password" className="sr-only">
-                                Contraseña
-                      </label>
+                        <div className="">
+                            <label htmlFor="password" className="sr-only">Contraseña</label>
                             <input
                                 id="password"
                                 name="password"
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm my-2"
                                 placeholder="Contraseña"
+                            />
+                        </div>
+                        <div className="">
+                            <label htmlFor="password" className="sr-only">
+                                Confirmar contraseña
+                            </label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="appearance-none rounded-none relative block w-full px-3 py-3 border border-pink-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm my-2"
+                                placeholder="Confirmar contraseña"
                             />
                         </div>
                     </div>
 
                     <div className="flex items-center justify-between">
                         <div className="text-sm">
-                            <a href="#" className="font-medium text-pink-600 hover:text-pink-500">
-                                ¿No tienes cuenta? - Registrate
-                      </a>
+                            <Link to="/login" className="font-medium text-pink-600 hover:text-pink-500">
+                                ¿Ya tienes cuenta? - Inicia sesion
+                            </Link>
                         </div>
                     </div>
 
@@ -58,7 +84,7 @@ export default function Register() {
                             <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                                 <LockClosedIcon className="h-5 w-5 text-pink-500 group-hover:text-indigo-400" aria-hidden="true" />
                             </span>
-                      Entrar
+                            Entrar
                     </button>
                     </div>
                 </form>
