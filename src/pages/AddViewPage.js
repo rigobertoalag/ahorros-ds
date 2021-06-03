10710725b550109598e1e3e9bb1a1b48ea449fea
rigobertoalag <a20150338@utem.edu.mx>
2021-06-03 18:59:17 -0500

    Save progress 03/06

--- src/pages/AddViewPage.js
+++ src/pages/AddViewPage.js
@@ -1,44 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Pork } from '../assets/pork.svg';
-import {PresentationChartBarIcon, BookOpenIcon, PlusIcon, SwitchHorizontalIcon } from '@heroicons/react/outline'
+import { PresentationChartBarIcon, BookOpenIcon, PlusIcon, SwitchHorizontalIcon } from '@heroicons/react/outline'
 
 export default function AddViewPage() {
     return (
-        <div class="grid grid-cols-2 justify-items-center mt-20">
-            <div className="pl-5 mb-28">
+        <div class="grid grid-cols-2 justify-items-center mt-20 table-auto">
+            <div className="bg-green-900 w-full">
                 <Link to="/add-ahorro">
-                    <Pork />
-                    <div className="absolute bottom-0 left-0"></div>
-                    <PlusIcon className="absolute top-20 left-4 h-16 text-green-500"/>
-                    Añadir ahorro
+                    <div className="w-3/4 h-3/4 grid grid-cols-1 justify-items-center">
+                        <Pork />
+                        <PlusIcon className="h-16 text-green-500" />
+                        Añadir ahorro
+                </div>
                 </Link>
             </div>
-            <div className="pr-5">
+            <div className="my-10">
                 <Link to="/rmv-ahorro">
                     <Pork />
-                    <SwitchHorizontalIcon className="absolute top-24 right-40 h-12 text-red-500"/>
+                    {/*<SwitchHorizontalIcon className="absolute top-32 right-40 h-12 text-red-500" />*/}
                     Retirar ahorro
                 </Link>
             </div>
 
-            <div className="pl-20">
+            <div className="text-center my-10">
                 <Link to="/list-ahorros">
-                    <BookOpenIcon className="h-2/3 w-2/3 text-pink-500"/>
-                    Ver ahorros
+                    <BookOpenIcon className="text-pink-500" />
+                    <span>Ver ahorros</span>
                 </Link>
             </div>
-            {/*<div className="pr-20 pl-10">
-                <Link to="/all-ahorros">
-                    <PresentationChartBarIcon className="h-2/3 w-2/3 text-pink-500"/>
-                    Todos los movimientos
-                </Link>
-            </div>*/}
-
-            <div className="border-l-4 bg-gray-900 h-2/3 absolute left-1/2 -ml-1 top-20"></div>
-            <div className="bg-gray-900 w-1/4 absolute border-2 top-1/2 left-2/4"></div>
-            <div className="bg-gray-900 w-1/4 absolute border-2 top-1/2 right-2/4"></div>
         </div >
-
     )
 }
